1ec241fa46a4259798e0095986b6f9c30a80f946
haoyu <popduke@gmail.com>
2016-08-09 00:05:27 +0800

    save progress

---
+++ Tests/GenerateProblemTests.swift
@@ -0,0 +1,28 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by HaoYu on 16/8/6.
+//  Copyright © 2016年 example. All rights reserved.
+//
+
+import XCTest
+import Nimble
+import RandomArithmetics
+
+class Tests: XCTestCase {
+    
+    func testNextAddtion() {
+        let additionProblem = nextAddition()
+        additionProblem
+        expect(additionProblem.leftOperand).to()
+    }
+    
+    func testNextMultiplication() {
+
+    }
+    
+    func testNextSubtraction() {
+        
+    }
+}
