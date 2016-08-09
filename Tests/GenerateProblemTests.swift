42b1ce38e111269c1a6a1397749cbc937daae02e
haoyu <haoy@jikre.com>
2016-08-09 23:41:56 +0800

    save progress

--- Tests/GenerateProblemTests.swift
+++ Tests/GenerateProblemTests.swift
@@ -2,27 +2,43 @@
 //  Tests.swift
 //  Tests
 //
-//  Created by HaoYu on 16/8/6.
+//  Created by Yonny Hao on 16/8/6.
 //  Copyright © 2016年 example. All rights reserved.
 //
 
 import XCTest
 import Nimble
-import RandomArithmetics
+@testable import RandomArithmetics
 
 class Tests: XCTestCase {
     
     func testNextAddtion() {
         let additionProblem = nextAddition()
         additionProblem
-        expect(additionProblem.leftOperand).to()
+        expect(additionProblem.leftOperand).to(beLessThanOrEqualTo(20))
+        expect(additionProblem.leftOperand).to(beGreaterThan(0))
+        expect(additionProblem.rightOperand).to(beLessThanOrEqualTo(20))
+        expect(additionProblem.rightOperand).to(beGreaterThan(0))
+        expect(additionProblem.op).to(equal(Operator.Add))
     }
     
     func testNextMultiplication() {
-
+        let multiplicationProblem = nextMultiplication()
+        multiplicationProblem
+        expect(multiplicationProblem.leftOperand).to(beLessThanOrEqualTo(9))
+        expect(multiplicationProblem.leftOperand).to(beGreaterThan(0))
+        expect(multiplicationProblem.rightOperand).to(beLessThanOrEqualTo(9))
+        expect(multiplicationProblem.leftOperand).to(beGreaterThan(0))
+        expect(multiplicationProblem.op).to(equal(Operator.Multiply))
     }
     
     func testNextSubtraction() {
-        
+        let minusProblem = nextSubtraction()
+        minusProblem
+        expect(minusProblem.leftOperand).to(beLessThanOrEqualTo(20))
+        expect(minusProblem.leftOperand).to(beGreaterThan(0))
+        expect(minusProblem.rightOperand).to(beLessThanOrEqualTo(20))
+        expect(minusProblem.rightOperand).to(beGreaterThan(0))
+        expect(minusProblem.op).to(equal(Operator.Minus))
     }
 }
